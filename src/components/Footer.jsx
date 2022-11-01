6078cc620771cb7ee0753c61da715d59124991fb
Tobiloba Orekoya <tobilobaorekoya19@gmail.com>
2022-11-01 02:08:37 +0100

    working on the footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
 function Footer() {
-  const currentYear = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {currentYear}</p>
+      <div class="footer-items">
+        <div className="footer-item">
+          <img src="img/zuri.svg" alt="" />
+        </div>
+        <div className="footer-item">
+          <p>HNG Internship 9 Frontend Task</p>
+        </div>
+        <div className="footer-item">
+          <img src="img/I4G.svg" alt="" />
+        </div>
+      </div>
     </footer>
   );
 }
